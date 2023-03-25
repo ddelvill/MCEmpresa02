d9cfe5b0b469e4cc8145c7c01dc437b6714d0302
=ddelvill <dayanasdelvillar@gmail.com>
2023-03-25 18:07:04 +0100

    Estado inicial

---
+++ src/main/java/com/example/McEmpresa02Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McEmpresa02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McEmpresa02Application.class, args);
+	}
+
+}
